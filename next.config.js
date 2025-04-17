ea310861861bc4dbd02730ae14138fc1687dfd26
jincheng_m <jincheng_m@infi.us>
2025-04-17 14:25:33 -0500

    fix: Troubleshooting the default example page on Netlify

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   output: "export",
-  distDir: ".next",
+  distDir: "out",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   // 添加Netlify部署中禁用revalidation路径的配置
-  exportPathMap: async function (
-    defaultPathMap: Record<string, { page: string }>
-  ) {
+  exportPathMap: async function (defaultPathMap) {
     // 从默认路径映射中删除/revalidation路径
     delete defaultPathMap["/revalidation"];
 
@@ -18,4 +19,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
